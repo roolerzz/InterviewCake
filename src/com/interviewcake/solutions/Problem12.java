33a3a3907a5c42879f641297161a1df7915a89ee
Hemant Sethi <sethi.hemant@gmail.com>
2018-03-10 21:29:20 -0500

    Solved: Find Rotation point in an Array.

---
+++ src/com/interviewcake/solutions/Problem12.java
@@ -0,0 +1,31 @@
+package com.interviewcake.solutions;
+
+public class Problem12 {
+
+	public static int findRotatedIndex(String[] words) {
+		// Sanitize the input.
+		if(words == null || words.length <=1)
+			throw new IllegalArgumentException("Rotations can be performed with atleast 2 elements.");
+		
+		String firstWord = words[0];
+		int floorIndex = 0, ceilIndex = words.length-1;
+		int midIndex;
+		while (floorIndex < ceilIndex) {
+			midIndex = floorIndex + ((ceilIndex-floorIndex)/2);
+			if(words[midIndex].compareTo(firstWord)>=0) {
+				floorIndex = midIndex;
+			}
+			else {
+				ceilIndex = midIndex;
+			}
+			
+			if(floorIndex == ceilIndex-1)break;
+		}
+		
+		// Being careful if the array is not rotated, then above algo gives incorrect result.
+		
+		if(words[floorIndex].compareTo(words[ceilIndex])<=0)
+			ceilIndex = floorIndex;
+		return ceilIndex;
+	}
+}
